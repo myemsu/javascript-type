711370586ad7d6408bec91ecd5b0befc0ba9c328
myemsu <706688@naver.com>
2021-08-14 01:41:14 +0900

    typescript

---
+++ index.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var sum = add(1, 3);
+console.log(sum);
---
+++ index.ts
@@ -0,0 +1,6 @@
+function add ( a: number, b: number ) {
+    return a + b;
+}
+
+let sum: number = add(1, 3);
+console.log(sum);
